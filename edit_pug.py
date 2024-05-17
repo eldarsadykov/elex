3b170abe557c9c061b9dcbd2c4dd53c0c4aa3363
Eldar Sadykov <88986450+eldarsadykov@users.noreply.github.com>
2024-05-17 17:38:51 +0200

    Update edit_pug.py

--- edit_pug.py
+++ edit_pug.py
@@ -4,13 +4,6 @@ import re
 directory_path = "pug"
 
 
-def replace_titles_with_variables(file):
-    input_text = file.read()
-    pattern = r"h1 (.+)"
-    output_text = re.sub(pattern, r'- const title = "\1"\n\th1= title', input_text)
-    return output_text
-
-
 def process_text(file):
     input_text = file.read()
     pattern = r"h1 (.+)"
@@ -18,21 +11,22 @@ def process_text(file):
     return output_text
 
 
-for filename in os.listdir(directory_path):
-    # Construct full file path
-    file_path = os.path.join(directory_path, filename)
-    # Check if it's a file
-    if os.path.isfile(file_path):
-        print(f"File: {filename}")
-    else:
-        print(f"Skipping directory: {filename}")
-        continue
-
-    # Read all lines from the file
+def process_file(file_path):
     with open(file_path, "r") as file:
-
         output_text = process_text(file)
 
-    # Write the remaining lines back to the file
     with open(file_path, "w") as file:
         file.write(output_text)
+
+
+def process_directory(directory_path):
+    for filename in os.listdir(directory_path):
+        file_path = os.path.join(directory_path, filename)
+
+        if os.path.isfile(file_path):
+            print(f"File: {filename}")
+        else:
+            print(f"Skipping directory: {filename}")
+            continue
+
+        process_file(file_path)
