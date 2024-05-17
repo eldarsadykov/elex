9a2ceadacf152960643935ff5c3c777b58a2a23e
Eldar Sadykov <88986450+eldarsadykov@users.noreply.github.com>
2024-05-17 17:57:51 +0200

    variables

--- edit_pug.py
+++ edit_pug.py
@@ -30,3 +30,31 @@ def process_directory(directory_path):
             continue
 
         process_file(file_path)
+
+
+def get_js_array_of_titles():
+    filename = "inhaltsverzeichnis.pug"
+    file_path = os.path.join(directory_path, filename)
+    with open(file_path, "r") as file:
+        text = file.read()
+    # Regular expression pattern to match the desired parts
+    pattern = r'\|\s*(\d+)\.\s*a\(href="([^"]+)"\)\s*(.+)'
+
+    # Find all matches in the text
+    matches = re.findall(pattern, text)
+
+    # Initialize the JS dictionary
+    js_dict = "const titles = {\n"
+
+    # Construct the JS dictionary entries
+    for match in matches:
+        index, address, title = match
+        js_dict += (
+            f'    "{title}": {{ "index": {int(index)}, "address": "{address}" }},\n'
+        )
+
+    # Close the JS dictionary
+    js_dict += "};"
+
+    # Print the resulting JS dictionary
+    print(js_dict)
