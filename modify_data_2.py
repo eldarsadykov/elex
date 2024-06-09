24af0b69a4eaf020963499ba3574627e266e8fca
Eldar Sadykov <88986450+eldarsadykov@users.noreply.github.com>
2024-06-09 15:37:52 +0200

    link source and target

--- modify_data_2.py
+++ modify_data_2.py
@@ -5,6 +5,18 @@ os.system("clear")
 
 with open("data_2.json", "r") as file:
     data = json.load(file)
+    import json
+
+    for chapter in data["chapters"]:
+        chapter["main_route_link_source"] = any(
+            link["main_route_link"] for link in chapter["links"]
+        )
+        chapter["main_route_link_target"] = any(
+            link["main_route_link"]
+            for other_chapter in data["chapters"]
+            for link in other_chapter["links"]
+            if link["target"] == chapter["title"]
+        )
 
 
 with open("data_3.json", "w") as file:
